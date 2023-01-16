995d10c04a5358722c2a7cbfdc3d9b6486f3fb3a
rafaelpontes13@hotmail.com <Rafa91197353>
2023-01-16 20:02:13 -0300

    init

---
+++ src/main/java/com/example/app/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.example.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+
+    public static final String MESSAGE_KEY = "message";
+    public static final String HOSTNAME_KEY = "hostname";
+    public static final String IP_KEY = "ip";
+
+    @GetMapping("/")
+    public Map<String, String> helloWorld() throws UnknownHostException {
+        return getResponse();
+    }
+
+    private Map<String, String> getResponse() throws UnknownHostException {
+        String host = InetAddress.getLocalHost().getHostName();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        Map<String, String> response = new HashMap<>();
+        response.put(MESSAGE_KEY, "Hello World!");
+        response.put(HOSTNAME_KEY, host);
+        response.put(IP_KEY, ip);
+        return response;
+    }
+
+}
